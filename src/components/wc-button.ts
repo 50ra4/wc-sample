ebd47466680ea078825c6e01a2897a472c9fefc5
50ra4 <50ra4.dev.dev@gmail.com>
2022-06-01 23:04:06 +0900

    apply: global css to wc-button

--- src/components/wc-button.ts
+++ src/components/wc-button.ts
@@ -1,23 +1,7 @@
 import { LitElement, css, html } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 import { styleMap } from 'lit/directives/style-map.js';
-
-const BUTTON_COLOR = {
-  success: {
-    font: '#fafafa',
-    background: '#2e7c31',
-  },
-  error: {
-    font: '#fafafa',
-    background: '#c62828',
-  },
-  warning: {
-    font: '#212121',
-    background: '#fdd835',
-  },
-} as const;
-
-type ButtonColor = keyof typeof BUTTON_COLOR;
+import { ButtonColor, BUTTON_COLOR, RESET_CSS } from '../styles';
 
 /**
  * wc-button
@@ -26,11 +10,14 @@ type ButtonColor = keyof typeof BUTTON_COLOR;
  */
 @customElement('wc-button')
 export class WCButton extends LitElement {
-  static override styles = css`
-    :host {
-      min-width: 180px;
-    }
-  `;
+  static override styles = [
+    ...RESET_CSS,
+    css`
+      :host {
+        min-width: 180px;
+      }
+    `,
+  ];
 
   /**
    * Buttonの色を指定する（success、error、warning）
@@ -41,7 +28,7 @@ export class WCButton extends LitElement {
   override render() {
     const buttonStyle = styleMap({
       'background-color': BUTTON_COLOR[this.color].background,
-      'font-color': BUTTON_COLOR[this.color].font,
+      color: BUTTON_COLOR[this.color].font,
     });
 
     return html`
