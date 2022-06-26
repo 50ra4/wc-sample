0c103d81e9dd1c24361c6e3287441b4056f9fcfd
50ra4 <50ra4.dev.dev@gmail.com>
2022-06-26 22:28:27 +0900

    update: implement todo list controller functions

--- src/controllers/todo-list-controller.ts
+++ src/controllers/todo-list-controller.ts
@@ -7,7 +7,7 @@ type Todo = {
   createdAt: number;
 };
 
-// const createId = () => `${new Date().getTime() * Math.random()}`;
+const createId = () => `${new Date().getTime() * Math.random()}`;
 
 export class TodoListController implements ReactiveController {
   private _data: Todo[] = [];
@@ -28,25 +28,41 @@ export class TodoListController implements ReactiveController {
   /**
    * TodoをlocalStorageから復元する
    */
-  public restoreTodoList(key: string) {}
+  public restoreTodoList(key: string) {
+    this._data = [...(JSON.parse(window.localStorage.getItem(key) ?? '[]') as Todo[])];
+  }
 
   /**
    * TodoをlocalStorageに保存する
    */
-  public saveTodoList(key: string) {}
+  public saveTodoList(key: string) {
+    window.localStorage.setItem(key, JSON.stringify(this.data));
+    window.alert('completed to save.');
+  }
 
   /**
    * Todoを新しく追加する
    */
-  public createTodo(text: string) {}
+  public createTodo(text: string) {
+    this._data = [
+      ...this._data,
+      { id: createId(), text: text, completed: false, createdAt: new Date().getTime() },
+    ];
+  }
 
   /**
    * Todoを完了/未完了に切り替える
    */
-  public toggleTodoCompleted(id: string) {}
+  public toggleTodoCompleted(id: string) {
+    this._data = this.data.map((todo) =>
+      todo.id !== id ? todo : { ...todo, completed: !todo.completed },
+    );
+  }
 
   /**
    * Todoを全て削除する
    */
-  public clearTodoList() {}
+  public clearTodoList() {
+    this._data = [];
+  }
 }
